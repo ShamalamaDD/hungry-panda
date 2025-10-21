6c297fb6ca97f0093ff95c49352f9d70dec0a086
Mitheel Ramdaw <mitheelramdaw@gmail.com>
2025-10-21 23:10:25 +0200

    chore : health check endpoint created

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/", include_in_schema=False)
 def read_root():
     return RedirectResponse(url="/docs")
+
+@app.get("/pint", tags=['Health Check'])
+def ping():
+    return {"status" : "Success", "message" : "Shamalama ding dong!"}
